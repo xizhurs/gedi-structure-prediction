61a07eb937ad65b48227cf0d2ae10823f4a7312f
Xi <xi.zhu@rabobank.com>
2025-07-31 19:18:39 +0200

    add pretraining

--- src/pretrain.py
+++ src/pretrain.py
@@ -1,16 +1,15 @@
 import pickle
 import torch
 import lightning as L
-from pathlib import Path
 from glob import glob
 from sklearn.model_selection import train_test_split
 from src.data_loader import create_tiledataloader_split
-from src.model.unet import Mid_fusion_UNetRegression, FreezeEncoderCallback
 from lightning.pytorch.callbacks import (
     ModelCheckpoint,
     EarlyStopping,
     LearningRateMonitor,
 )
+from src.model.mae import MAEUNetPretrain
 
 
 def get_device():
@@ -84,66 +83,39 @@ def get_dataloader(
     return (train_loader, val_loader, test_loader)
 
 
-def get_pretrained_weights(
-    s2_file,
-    s1_file,
-):
-
-    s2_checkpoint = torch.load(s2_file, map_location=torch.device(device))
-    pretrained_state_s2 = s2_checkpoint["state_dict"]
-    s2_encoder_state = {
-        k.replace("encoder.", ""): v
-        for k, v in pretrained_state_s2.items()
-        if k.startswith("encoder.")
-    }
-    s1_checkpoint = torch.load(s1_file, map_location=torch.device(device))
-    pretrained_state_s1 = s1_checkpoint["state_dict"]
-    s1_encoder_state = {
-        k.replace("encoder.", ""): v
-        for k, v in pretrained_state_s1.items()
-        if k.startswith("encoder.")
-    }
-    return (s2_encoder_state, s1_encoder_state)
-
-
 def get_model(
-    s2_weights=None,
-    s1_weights=None,
     lr=1e-3,
     lr_decay=0.1,
-    unfreeze_epoch=10,
     weight_decay=1e-4,
+    sensor_train="s2",
 ):
-    model_regressor = Mid_fusion_UNetRegression(
-        in_channels_s2=20,
-        in_channels_s1=6,
-        in_channels_dem=2,
-        out_channels=3,
+    model = MAEUNetPretrain(
+        in_channels=20,
+        dimensions=2,
+        out_channels_first_layer=16,
+        conv_num_in_layer=[2, 2, 2, 2],
+        kernel_size=5,
+        normalization="Batch",
+        downsampling_type="max",
+        residual=True,
+        padding_mode="zeros",
+        activation="LeakyReLU",
+        upsampling_type="conv",
+        use_bias=True,
+        use_sigmoid=False,
         lr=lr,
         lr_decay=lr_decay,
-        total_iters=unfreeze_epoch,
         weight_decay=weight_decay,
-        s2_weights=s2_weights,
-        s1_weights=s1_weights,
-        dem_weights=None,
+        patch_size=128,
+        mask_ratio=0.75,
+        mask_channels=True,
+        sensor_train=sensor_train,
     )
-    return model_regressor
+    return model
 
 
-def fine_tuning(
-    unfreeze_epoch=10,
-    max_epochs=100,
-    lr=1e-3,
-    lr_decay=0.1,
-    weight_decay=1e-4,
-    s2_file=Path.cwd()
-    / "experiments"
-    / "weights"
-    / "biomass-unet-s2-pretrain-epoch=98-val_loss=0.05282.ckpt",
-    s1_file=Path.cwd()
-    / "experiments"
-    / "weights"
-    / "biomass-unet-s1alos-pretrain-epoch=98-val_loss=0.06877.ckpt",
+def pretrain(
+    max_epochs=100, lr=1e-3, lr_decay=0.1, weight_decay=1e-4, sensor_train="s2"
 ):
     (train_loader, val_loader, test_loader) = get_dataloader(
         dire="data/processed",
@@ -151,22 +123,18 @@ def fine_tuning(
         batch_size=2,
     )
 
-    (s2_encoder_state, s1_encoder_state) = get_pretrained_weights(s2_file, s1_file)
     model_regressor = get_model(
-        s2_weights=s2_encoder_state,
-        s1_weights=s1_encoder_state,
         lr=lr,
         lr_decay=lr_decay,
-        unfreeze_epoch=unfreeze_epoch,
         weight_decay=weight_decay,
+        sensor_train=sensor_train,
     )
 
-    unfreeze_callback = FreezeEncoderCallback(unfreeze_epoch=unfreeze_epoch)
     lr_monitor = LearningRateMonitor(logging_interval="epoch")
 
     path_experiment = "experiments/tests"
 
-    name = "unet-gedi-mid-fusion-testing"
+    name = "unet-gedi-pretrain-testing"
 
     trainer = L.Trainer(
         accelerator="auto",
@@ -184,7 +152,6 @@ def fine_tuning(
                 monitor="val_loss",
             ),
             EarlyStopping(monitor="val_loss", mode="min", patience=30),
-            unfreeze_callback,
             lr_monitor,
         ],
         enable_progress_bar=True,
@@ -194,10 +161,6 @@ def fine_tuning(
 
 
 if __name__ == "__main__":
-    fine_tuning(
-        unfreeze_epoch=10,
-        max_epochs=100,
-        lr=1e-3,
-        lr_decay=0.1,
-        weight_decay=1e-4,
+    pretrain(
+        max_epochs=100, lr=1e-3, lr_decay=0.1, weight_decay=1e-4, sensor_train="s2"
     )
